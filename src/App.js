354e2a45a4bf075a6bb0bfb8f10e940ca67a7d06
pfernandes <pj_fernandes@id.uff.br>
2023-03-13 18:37:51 -0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import './App.css';
-import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import Courses from './components/Courses';
 import ExemploQuery from './components/ExemploQuery';
 import NotFoundRoute from './components/NotFoundRoute';
+
+
 function App() {
   return (
     <Router>
+
+
       <div>
         <NavBar />
-        <Switch>
+        <Routes>
           <Route exact path="/"
-            component={Home} />
+            element={<Home/>} />
           <Route path="/about"
-            component={About} />
+            element={<About/>} />
           <Route path="/courses"
-            component={Courses} />
+            element={<Courses/>} />
           <Route path="/query"
-            component={ExemploQuery} />
+            element={<ExemploQuery/>} />
           <Route path="*"
-            component={NotFoundRoute} />
-        </Switch>
+            element={<NotFoundRoute/>} />
+        </Routes>
       </div>
+
     </Router>
   );
 }
