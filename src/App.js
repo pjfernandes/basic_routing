8642b15c2af9d732f93fa26884089e29255211d6
pfernandes <pj_fernandes@id.uff.br>
2023-03-13 18:27:57 -0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes as Switch, Route } from
-  'react-router-dom';
+import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
